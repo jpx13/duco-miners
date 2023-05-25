b45371f1b7bcb961b41fa06ebc7d1062cf251cf9
jpx13 <jp.xiume@aliceadsl.fr>
2023-05-25 17:26:52 +0200

    Add files via upload

--- miners2.py
+++ miners2.py
@@ -34,7 +34,7 @@ def main():
     subprocess.call('clear', shell = True )
     tab_moy = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
     indice  = 0
-    username = "discopepereland"
+    username = "put your Duco username here"
     while not username:
         username = input('Enter your DUCO username: ')
 
